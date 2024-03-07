0978391da3745dc5fc1e99f79c280a039aedf162
michaell741 <michaell741@nycstudents.net>
2024-03-07 12:28:25 -0500

    yay

---
+++ gradebook copy.py
@@ -0,0 +1,31 @@
+student = { 
+    "Name": 'Michael Lee',
+    "Class": '27F',
+    "Grade": []
+}
+studentlist = student['name']
+def students():
+    students = (input("Enter Student's Name"))
+    studentlist.append(students)
+    students = (input("Enter Student's Class"))
+
+gradelist = student['Grade']
+def Grade():
+    Grade = int(input('what is your grade?')) 
+    gradelist.append(Grade)
+    moregrade = input("Want to enter more grades? Y/N")
+    if moregrade == 'Y':
+        Grade()
+    else:
+        return 
+    
+
+    length = len(student['Grade'])
+    gradetotal = sum(student['Grade'])
+    avg = gradetotal/length
+    print(avg)
+
+Create_new_student = {
+                        'Name'
+}
+
