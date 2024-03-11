08ea0cf6a66dff13748919dc45a390a5a34f1f9b
michaell741 <michaell741@nycstudents.net>
2024-03-11 12:28:07 -0400

    dfg

--- list copy.py
+++ list copy.py
@@ -1,7 +1,6 @@
 accounts = [["1", "2", "3"], ["5", "6", "7"], ["1", "2"]]
-y = [[int(i) for i in x] for x in accounts]
-a = (sum(y[0]))
-b = (sum(y[1]))
-c = (sum(y[2]))
+def maxMoney(accounts):
+accounts= [[int(i) for i in x] for x in accounts]
 
-print(sum(a,b))
+
+print(avg)
