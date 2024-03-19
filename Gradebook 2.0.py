4d65d6b07822bcb11c609fc98fd9d80a582ae1aa
michaell741 <michaell741@nycstudents.net>
2024-03-19 12:29:32 -0400

    ads

--- Gradebook 2.0.py
+++ Gradebook 2.0.py
@@ -1,4 +1,19 @@
-createstudent = "y"
-while createstudent == "Y":
-    name = (input( "what is your name"))
+students = []
+
+def avg(grades):
+    return sum(grades)/len(grades)
+def createstudent(name, classroom, grades):
+    avg = avg(grades)
+    return {"Name": name, "classroom": classroom, "Grades":grades, "Student Average": avg}
+
+
+createmorestudents = "Yes"
+while createmorestudents == "Yes":
+    studentname = (input("Enter Student's Name"))
+    studentclass = (input("Enter Student's class"))
+    studentgrades = [int(i) for i in input("Enter Student's class")]
+    newstudent = createmorestudents(studentname, studentclass, )
+
+
+
     
